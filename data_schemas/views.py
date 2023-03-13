8f4fc9684cae544be7ac4f2ad9a1a49d7daf477f
NazarNazaryshyn <n.nazaryshyn@gmail.com>
2023-03-13 12:12:17 +0200

    improvements

--- data_schemas/views.py
+++ data_schemas/views.py
@@ -9,6 +9,8 @@ from data_schemas.models import Schema, SchemaColumn, DataType, DataSet
 import csv
 import uuid
 
+import random
+
 
 csv_character = {
     'DoubleQuote': '"',
@@ -17,6 +19,36 @@ csv_character = {
     'Tab': "\t",
     'SemiColon': ';',
     'Pipe': '|'
+}
+
+
+data_types_random_value = {
+    "Date": ["May 10, 1992", "August 3, 1987", "November 22, 2001", "July 17, 1998", "January 4, 2004",
+             "September 19, 1995", "March 8, 1989", "December 12, 2008", "April 29, 1991", "October 2, 1997"],
+    "Address": ["1234 Main Street, Anytown, USA", "5678 Elm Street, Somewhere, USA", "9101 Pine Avenue, Anywhere, USA",
+                "2345 Maple Road, Nowhere, USA", "6789 Oak Lane, Everywhere, USA", "1111 Cherry Street, Here, USA",
+                "2222 Magnolia Avenue, There, USA", "3333 Cedar Boulevard, Anyplace, USA", "4444 Birch Lane, Someplace, USA",
+                "5555 Willow Way, Everywhere, USA"],
+    "Integer": [],
+    "Text": ["The quick brown fox jumps over the lazy dog.", "A penny saved is a penny earned.",
+             "The early bird catches the worm.", "All work and no play makes Jack a dull boy.",
+             "The sky is the limit.", "Actions speak louder than words.",
+             "You can't judge a book by its cover.", "When life gives you lemons, make lemonade."],
+    "Company name": ["Facebook", "Google", "Amazon", "SparkleTech", "BluePeak", "SwiftWorks",
+                     "GreenScape", "DreamWorks Inc.", "Silverline Solutions", "BrightStar Corporation"],
+    "Phone number": ["+380 67 123 4567", "+380 63 987 6543", "+380 99 456 7890", "+380 98 765 4321",
+                     "+380 73 456 7891", "+380 66 789 0123", "+380 50 123 4567", "+380 68 901 2345"],
+    "Domain name": ["sparrowcloud.com", "bluemermaid.net", "greentiger.org", "yellowbutterfly.io",
+                    "silverdolphin.co", "redfoxtech.biz", "purpleowlpro.com", "orangecatstudios.net",
+                    "bluewhalecreative.org", "goldenlionagency.co"],
+    "Email": ["johnsmith85@gmail.com", "sarahbrown23@yahoo.com", "davidlee42@hotmail.com",
+              "emilyjones98@outlook.com", "robertwilson76@gmail.com", "jenniferbaker87@icloud.com",
+              "adamclark54@yahoo.com", "lauramiller32@hotmail.com", "kevinwhite61@outlook.com",
+              "melissagreen19@gmail.com"],
+    "Job": ["Accountant", "Software Developer", "Graphic Designer", "Lawyer", "Sales Representative",
+            "Chef", "Nurse", "Marketing Manager", "Electrician", "Physical Therapist"],
+    "Full name": ["Michaela Thompson", "William Rodriguez", "Natasha Patel", "Benjamin Lee", "Olivia Carter",
+                  "Isaac Davis", "Emma Jackson", "Aaron Cooper", "Samantha Wright", "Christopher Kim"]
 }
 
 
@@ -65,37 +97,40 @@ def generate_data(request):
     schema = Schema.objects.filter(id=schema_id).first()
     schema_cols = SchemaColumn.objects.filter(schema_id=schema_id).order_by('order').all()
 
-    items_list = []
-    for item in range(rows):
-        unique_id = uuid.uuid4()
+    unique_id = uuid.uuid4()
 
-        new_data_set = DataSet(
-            status=False,
-            schema=schema,
-            unique_id=unique_id
-        )
-        new_data_set.save()
+    new_data_set = DataSet(
+        status=False,
+        schema=schema,
+        unique_id=unique_id
+    )
+    new_data_set.save()
 
-        items_list.append({"id": item + 1,
-                           "created_at": new_data_set.created_at,
-                           "status": new_data_set.status,
-                           "unique_id": new_data_set.unique_id})
+    import time
+    time.sleep(0.5)
 
-        import time
-        time.sleep(0.5)
+    csv_headers = [header.column_name for header in schema_cols]
+    data_type_rows = [type.data_type for type in schema_cols]
+    with open(f'media/{unique_id}.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f, delimiter=csv_character[column_separator],
+                            quotechar=csv_character[string_character],
+                            quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["#", *csv_headers])
+        for index in range(rows):
+            for _ in schema_cols:
+                row = [random.choice(data_types_random_value[str(header)]) if str(header) != "Integer" else random.randint(18, 60) for header in data_type_rows]
+            writer.writerow([index, *row])
 
-        with open(f'media/{unique_id}.csv', 'w', encoding='UTF8', newline='') as f:
-            writer = csv.writer(f, delimiter=csv_character[column_separator],
-                                quotechar=csv_character[string_character],
-                                quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["#", "Column name", "Column type"])
-            for index, item in enumerate(schema_cols):
-                writer.writerow([index + 1, item.column_name, item.data_type])
+    new_data_set.status = True
+    new_data_set.save()
 
-        new_data_set.status = True
-        new_data_set.save()
+    data = {
+        "created_at": new_data_set.created_at,
+        "status": new_data_set.status,
+        "unique_id": new_data_set.unique_id
+    }
 
-    return JsonResponse({"status": items_list})
+    return JsonResponse({"status": data})
 
 
 def download_csv(request, unique_id):
